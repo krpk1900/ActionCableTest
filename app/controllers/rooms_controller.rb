565de7ee005c2a04f09b58d10860a10d6fd89294
krpk1900 <krpk1900@gmail.com>
2023-02-13 02:05:55 +0900

    接続数をリアルタイムで取得するように実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,13 @@
 class RoomsController < ApplicationController
   def show
     @messages = Message.all
+
+    @connection_num = ActionCable
+                      .server.open_connections_statistics
+                      .map { |con| con[:subscriptions]
+                      .map { |sub| JSON.parse(sub)["url"] } } # ここのurlを変えれば特定の接続数を取得できるはず
+                      .flatten
+                      .select { |url| url == 'http://localhost:3000' } # ここで特定のチャネル一致
+                      .size
   end
 end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,23 +1,29 @@
 import consumer from "channels/consumer"
 
-const appRoom = consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
+const appRoom = consumer.subscriptions.create(
+  {
+    channel: "RoomChannel",
+    url: location.origin
   },
+  {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    const messages = document.getElementById('messages');
-    messages.insertAdjacentHTML('beforeend', data['message']);
-  },
+    received(data) {
+      const messages = document.getElementById('messages');
+      messages.insertAdjacentHTML('beforeend', data['message']);
+    },
 
-  speak: function(message) {
-    return this.perform('speak', {message: message});
+    speak: function(message) {
+      return this.perform('speak', {message: message});
+    }
   }
-});
+);
 
 window.addEventListener("keypress", function(e) {
   if (e.keyCode === 13) {
